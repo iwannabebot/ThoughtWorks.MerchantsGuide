2e7c14b80132738a1b1f84c484567c5f87b8d7df
Nilay Vishwakarma <nilayvishwakarma@live.com>
2019-09-23 03:55:43 +0530

    tests

---
+++ tests/ThoughtWorks.MerchantsGuide.Test/MerchantsCompilerTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class MerchantsCompilerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ tests/ThoughtWorks.MerchantsGuide.Test/MerchantsInterpreterTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class MerchantsInterpreterTests
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
